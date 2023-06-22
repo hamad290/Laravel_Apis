e36edaad27251702d23fe386f3a3b22bc7fa7f99
Hammad Ur Rehman <100110573+hamad290@users.noreply.github.com>
2023-06-22 13:52:12 -0700

    Add files via upload

---
+++ app/Http/Controllers/AuthManager.php
@@ -0,0 +1,62 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+class AuthManager extends Controller
+{
+    function login(){
+        if(Auth::check()){
+            return redirect(route('home'));
+        }
+        return view('login');
+    }
+
+
+    function registration(){
+        if(Auth::check()){
+            return redirect(route('home'));
+        }
+        return view('registration');
+    }
+
+    function loginPost(Request $request){
+    $request->validate([
+        'email'=>'required',
+        'password'=>'required'
+    ]);
+    $credentails = $request->only('email','password');
+    if(Auth::attempt($credentails)){
+        return redirect()->intended(route('home'));
+    }
+    return redirect(route('login'))->with("error","login details are not valid");
+    }
+
+    function registrationPost(Request $request){
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required|email|unique:users',
+            'password'=>'required'
+        ]);
+        $data['name']= $request->name;
+        $data['email']= $request->email;
+        $data['password']=Hash::make($request->password);
+        $user = User::create($data);
+        if(!$user){
+            return redirect(route('registration'))->with("error","registration details are not valid");
+        }
+
+        return redirect(route('login'))->with("success","login details are not valid");
+    }
+
+    function logout(){
+        //Session::flush();
+        Auth::logout();
+        return redirect(route('login'));
+
+    }
+
+
+}
---
+++ storage/framework/views/88cc6e6671b0a23d3d1f6d95a98c90b9.php
@@ -0,0 +1,6 @@
+<?php $__env->startSection('title',"Home page"); ?>
+<?php $__env->startSection('content'); ?>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\laravel\custom_login\resources\views/welcome.blade.php ENDPATH**/ ?>
